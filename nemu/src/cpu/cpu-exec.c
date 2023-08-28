2e087203ab2ae30b4b31f78852d9b6efac2e119f
tracer-ics2022 <tracer@njuics.org>
2023-08-28 21:34:37 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-79-generic #86~20.04.2-Ubuntu SMP Mon Jul 17 23:27:17 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:34:37 up  1:45,  1 user,  load average: 0.08, 0.05, 0.01

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -40,10 +40,9 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
-// #ifdef CONFIG_WATCHPOINT
-//   if (ITRACE_COND) { log_write("%s\n", _this->logbuf); }
-// #endif
+#ifdef CONFIG_WATCHPOINT
   judge_watchpoint();
+#endif
 
 }
 
