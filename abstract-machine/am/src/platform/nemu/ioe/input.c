de4d30e987d41415dac6106c48f7953bec483373
tracer-ics2022 <tracer@njuics.org>
2023-09-19 21:56:17 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:56:17 up  4:58,  1 user,  load average: 0.08, 0.04, 0.07

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,13 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+  // kbd->keydown = 0;
+//my
+  if(kbd->keycode==AM_KEY_NONE){
+    return ;
+  }
+  inl(KBD_ADDR);
+
+  // kbd->keycode = AM_KEY_NONE;
+//
 }
