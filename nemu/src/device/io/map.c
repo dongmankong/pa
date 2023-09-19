14615b8452d1446bd5e0eb89eb096a5697831b79
tracer-ics2022 <tracer@njuics.org>
2023-09-19 21:20:48 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-83-generic #92~20.04.1-Ubuntu SMP Mon Aug 21 14:00:49 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:20:48 up  4:23,  1 user,  load average: 0.25, 0.22, 0.29

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -60,7 +60,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
   word_t ret = host_read(map->space + offset, len);
   
   // #ifdef
-    Log("name: %8s   lowAddr:0x%8x    highAddr:0x%8x",map->name,map->low,map->high);
+    Log("name: %-8s   lowAddr:0x%8x    highAddr:0x%8x",map->name,map->low,map->high);
   // #endif
   return ret;
 }
@@ -72,7 +72,7 @@ void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
   // #ifdef
-    Log("name: %-8s   lowAddr:0x%8x    highAddr:0x%8x",map->name,map->low,map->high);
+    Log("name:%-8s   lowAddr:0x%8x    highAddr:0x%8x",map->name,map->low,map->high);
   // #endif
 
 }
