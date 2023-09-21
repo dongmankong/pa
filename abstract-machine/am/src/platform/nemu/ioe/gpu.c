d9ff9324acdd248b1562ed2474bd88868adc8296
tracer-ics2022 <tracer@njuics.org>
2023-09-21 21:17:50 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:17:50 up  4:17,  1 user,  load average: 0.34, 0.19, 0.11

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -37,7 +37,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 //my
   if(ctl->sync==false){
     int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-    printf("%d  %d",x,y);
+    printf("%d  %d\n",x,y);
     // if (w == 0 || h == 0) return;
     // int block_size = w * h;
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
