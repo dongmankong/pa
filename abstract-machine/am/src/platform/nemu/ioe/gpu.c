03f4255fd8579c757349b0eb4e736c6bf361d3d3
tracer-ics2022 <tracer@njuics.org>
2023-09-21 20:38:56 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:38:56 up  3:38,  1 user,  load average: 0.16, 0.07, 0.02

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -5,16 +5,16 @@
 
 void __am_gpu_init() {
 //my
-  uint32_t i;
-  uint32_t w = inw(VGACTL_ADDR);  // TODO: get the correct width
-  uint32_t h = inw(VGACTL_ADDR+2);  // TODO: get the correct height
-  // int i;
-  // int w = 0;  // TODO: get the correct width
-  // int h = 0;  // TODO: get the correct height
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // uint32_t i;
+  // uint32_t w = inw(VGACTL_ADDR);  // TODO: get the correct width
+  // uint32_t h = inw(VGACTL_ADDR+2);  // TODO: get the correct height
+  // // int i;
+  // // int w = 0;  // TODO: get the correct width
+  // // int h = 0;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 //
 }
 
@@ -31,9 +31,22 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
+  // if (ctl->sync) {
+  //   outl(SYNC_ADDR, 1);
+  // }
+//my
+  if(ctl->sync){
+    int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+    if (w == 0 || h == 0) return;
+    int block_size = w * h;
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR+w*y+x;
+    uint32_t *p=ctl->pixels;
+    for(int i=0;i<block_size;++i){
+      fb[i]=*p;
+      p++;
+    }
   }
+//
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
