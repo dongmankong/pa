1e85b22dceedc0fd9f560873bc34a7e87fd74638
tracer-ics2022 <tracer@njuics.org>
2023-09-22 19:33:38 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:33:38 up  1:06,  1 user,  load average: 0.04, 0.42, 0.57

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -30,50 +30,37 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
     .vmemsz = 0
   };
 }
+
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  // if (ctl->sync) {
+  //   outl(SYNC_ADDR, 1);
+  // }
+//my
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   if (!ctl->sync && (w == 0 || h == 0)) return;
-  uint32_t *pixels = ctl->pixels;
+  // if(ctl->sync==false){
+    // printf("%d  %d\n",x,y);
+    // if (w == 0 || h == 0) return;
+    // int block_size = w * h;
+  uint32_t screen_w = (inl(VGACTL_ADDR))>>16;  
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
-  for (int i = y; i < y+h; i++) {
-    for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+  fb+=screen_w*y+x;
+  uint32_t *p=ctl->pixels;
+  for(int i=0;i<h;++i){
+    uint32_t *fb_first=fb;
+    for(int j=0;j<w;++j){
+      *fb_first=*p;
+      p++;
+      fb_first++;
     }
+    fb+=screen_w;
   }
+  // }  
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
+//
 }
-// void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-//   // if (ctl->sync) {
-//   //   outl(SYNC_ADDR, 1);
-//   // }
-// //my
-//   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
-//   if (!ctl->sync && (w == 0 || h == 0)) return;
-//   if(ctl->sync==false){
-//     // printf("%d  %d\n",x,y);
-//     // if (w == 0 || h == 0) return;
-//     // int block_size = w * h;
-//     uint32_t screen_w = (inl(VGACTL_ADDR))>>16;  
-//     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-//     fb+=screen_w*y+x;
-//     uint32_t *p=ctl->pixels;
-//     for(int i=0;i<h;++i){
-//       uint32_t *fb_first=fb;
-//       for(int j=0;j<w;++j){
-//         *fb_first=*p;
-//         p++;
-//         fb_first++;
-//       }
-//       fb+=screen_w;
-//     }
-//   }else{
-//     outl(SYNC_ADDR, 1);
-//   }
-// //
-// }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
   status->ready = true;
