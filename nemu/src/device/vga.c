c78a7a6b0ddf41c504a8da6e7d9716210826e081
tracer-ics2022 <tracer@njuics.org>
2023-09-22 22:03:43 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-84-generic #93~20.04.1-Ubuntu SMP Wed Sep 6 16:15:40 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:03:43 up  3:36,  1 user,  load average: 0.48, 0.21, 0.12

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -75,16 +75,12 @@ static inline void update_screen() {
 void vga_update_screen() {
   // TODO: call `update_screen()` when the sync register is non-zero,
   // then zero out the sync register
-  // uint32_t sync=mmio_read(CONFIG_VGA_CTL_MMIO +4,4);
-  // if(sync!=0){
-  //   update_screen();
-  //   mmio_write(CONFIG_VGA_CTL_MMIO +4,4,0);
-  // }
-    uint32_t sync = vgactl_port_base[1];
-  if (sync) {
+  uint32_t sync=mmio_read(CONFIG_VGA_CTL_MMIO +4,4);
+  if(sync!=0){
     update_screen();
-    vgactl_port_base[1] = 0;
+    mmio_write(CONFIG_VGA_CTL_MMIO +4,4,0);
   }
+  
 }
 //
 void init_vga() {
