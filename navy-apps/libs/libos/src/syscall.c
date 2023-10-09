a61cab5928276c1cccad3aea0b027d3f10b40f38
tracer-ics2022 <tracer@njuics.org>
2023-10-09 22:27:49 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:27:49 up  3:10,  1 user,  load average: 0.22, 0.10, 0.03

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -64,7 +64,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count) {
   // _exit(SYS_write);
 //my
-  _syscall_(SYS_write, fd, buf, count);
+  _syscall_(SYS_write, fd, (intptr_t)buf, count);
 //
   return 0;
 }
