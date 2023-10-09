dad24be942062aa6216924b797d691645cb096cd
tracer-ics2022 <tracer@njuics.org>
2023-10-09 21:06:31 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:31 up  1:49,  1 user,  load average: 0.19, 0.10, 0.10

--- abstract-machine/am/include/arch/riscv32-nemu.h
+++ abstract-machine/am/include/arch/riscv32-nemu.h
@@ -16,7 +16,7 @@ struct Context {
 };
 
 #define GPR1 gpr[17] // a7
-//my
+//my  "a0", "a1", "a2", "a0"
 #define GPR2 gpr[10]
 #define GPR3 gpr[11]
 #define GPR4 gpr[12]
--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,9 +6,10 @@ void do_syscall(Context *c) {
   a[0] = c->GPR1;
 //my
   // c->GPRx=0;
-  // a[2]=c->GPR3;
-  // a[3]=c->GPR4;
-  Log("a[0]: %x\n",a[0]);
+  a[1]=c->GPR2;
+  a[2]=c->GPR3;
+  a[3]=c->GPR4;
+  Log("a[0]: %x,a[1]:%x ,a[2]:%x,a[3]:%x   \n",a[0],a[1],a[2],a[3]);
 //
   switch (a[0]) {
 //my
