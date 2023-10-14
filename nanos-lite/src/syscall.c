b8a2678b0bd28e613e4cda57184cdca63be6c733
tracer-ics2022 <tracer@njuics.org>
2023-10-14 20:25:52 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:25:52 up 47 min,  1 user,  load average: 0.55, 0.39, 0.39

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,13 @@
 #include <common.h>
 #include "syscall.h"
 //my  a[0] type; a[1] a0;a[2] a1;a[3] a2
+//my
+int fs_open(const char *pathname, int flags, int mode);
+size_t fs_read(int fd, void *buf, size_t len);
+size_t fs_write(int fd, const void *buf, size_t len);
+size_t fs_lseek(int fd, size_t offset, int whence);
+int fs_close(int fd);
+//
 //
 void do_syscall(Context *c) {
   uintptr_t a[4];
@@ -10,7 +17,7 @@ void do_syscall(Context *c) {
   a[1]=c->GPR2;
   a[2]=c->GPR3;
   a[3]=c->GPR4;
-  // Log("a[0]: %x,a[1]:%x ,a[2]:%x,a[3]:%x   \n",a[0],a[1],a[2],a[3]);
+  Log("a[0]: %x,a[1]:%x ,a[2]:%x,a[3]:%x   \n",a[0],a[1],a[2],a[3]);
 //
   switch (a[0]) {
 //my
@@ -42,7 +49,9 @@ void do_syscall(Context *c) {
       c->GPRx=0;
       break;
 
-    // case SYS_open:
+    case SYS_open: //_syscall_(SYS_open, path, flags, mode);
+      c->GPRx=fs_open((char *)a[1], a[2], a[3]);
+      break;
 
 
 //
