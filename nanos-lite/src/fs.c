4f18374946a3f09444f42c7374b39db034f9a989
tracer-ics2022 <tracer@njuics.org>
2023-10-15 21:25:18 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:25:18 up  2:01,  1 user,  load average: 0.06, 0.07, 0.04

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -68,13 +68,13 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 
 size_t fs_read(int fd, void *buf, size_t len){
   // Log("进入read\n");
-  if(len==0) return 0;
-  size_t shengyu=file_table[fd].size-(file_table[fd].disk_offset-file_table[fd].start_offset);
-  // Log("%d\n",len);
-  if(shengyu<len){
-    Log("文件越界\n");
-    assert(0);
-  }
+  // if(len==0) return 0;
+  // size_t shengyu=file_table[fd].size-(file_table[fd].disk_offset-file_table[fd].start_offset);
+  // // Log("%d\n",len);
+  // if(shengyu<len){
+  //   Log("文件越界\n");
+  //   assert(0);
+  // }
   ramdisk_read(buf,file_table[fd].disk_offset,len);
   file_table[fd].disk_offset+=len;
   // Log("%x",*(uint32_t *)ehdr.e_ident);
@@ -86,12 +86,12 @@ size_t fs_read(int fd, void *buf, size_t len){
 size_t fs_write(int fd, const void *buf, size_t len){
   // Log("write\n");
   if(len==0) return 0;
-  size_t shengyu=file_table[fd].size-(file_table[fd].disk_offset-file_table[fd].start_offset);
-  // Log("%d\n",len);
-  if(shengyu<len){
-    Log("文件越界\n");
-    assert(0);
-  }
+  // size_t shengyu=file_table[fd].size-(file_table[fd].disk_offset-file_table[fd].start_offset);
+  // // Log("%d\n",len);
+  // if(shengyu<len){
+  //   Log("文件越界\n");
+  //   assert(0);
+  // }
   ramdisk_write(buf,file_table[fd].disk_offset,len);
   file_table[fd].disk_offset+=len;
   // Log("%x",*(uint32_t *)ehdr.e_ident);
