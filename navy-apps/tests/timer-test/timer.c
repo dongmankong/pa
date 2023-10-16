f35ebe7e7f8c7c9775a905500a71e15069826bfa
tracer-ics2022 <tracer@njuics.org>
2023-10-16 22:43:09 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:43:09 up  6:38,  1 user,  load average: 0.37, 0.17, 0.16

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -5,13 +5,13 @@ int main(void) {
   struct timeval tv;
   // struct timezone tz;
   gettimeofday(&tv, NULL);
-  __uint64_t ms = 500;
+  __uint64_t ms = 5000;
   while (1) {
     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
       gettimeofday(&tv, NULL);
     }
-    ms += 500;
-    printf("过了0.5s\n", ms);
+    ms += 5000;
+    printf("过了5s\n");
   }
   // printf("ms = %d\n", ms);
   return 0;
