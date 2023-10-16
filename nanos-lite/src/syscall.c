670ffdce1c73c55bf89aa130148c9c2beb8cc1e2
tracer-ics2022 <tracer@njuics.org>
2023-10-16 23:01:15 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:01:15 up  6:56,  1 user,  load average: 0.48, 0.36, 0.27

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -83,8 +83,10 @@ void do_syscall(Context *c) {
       uint64_t now= io_read(AM_TIMER_UPTIME).us;
       if(tv!=NULL){
         tv->tv_sec=now/(1000*1000);
-        // Log("%d\n",tv->tv_sec);
+        Log("%d\n",tv->tv_sec);
         tv->tv_usec=now%(1000*1000);
+        Log("%d\n", tv->tv_usec);
+
       }
 
       c->GPRx=0;
