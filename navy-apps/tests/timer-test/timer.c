807d780c24493520add7d049c97c620fffc8bed8
tracer-ics2022 <tracer@njuics.org>
2023-10-16 22:17:52 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:17:52 up  6:13,  1 user,  load average: 0.63, 0.34, 0.22

--- navy-apps/tests/timer-test/timer.c
+++ navy-apps/tests/timer-test/timer.c
@@ -7,7 +7,7 @@ int main(void) {
   gettimeofday(&tv, NULL);
   __uint64_t ms = 500;
   while (1) {
-    printf("ms = %d\n", ms);
+    // printf("ms = %d\n", ms);
     while ((tv.tv_sec * 1000 + tv.tv_usec / 1000) < ms) {
       gettimeofday(&tv, NULL);
     }
