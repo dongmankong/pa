2a153311d4a522568c4bf44f35d64f2fe54c96b6
tracer-ics2022 <tracer@njuics.org>
2023-10-16 23:04:06 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:04:06 up  6:59,  1 user,  load average: 0.49, 0.38, 0.29

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -134,7 +134,7 @@ off_t _lseek(int fd, off_t offset, int whence) {
 int _gettimeofday(struct timeval *tv, struct timezone *tz) {
   // Log("SYS_gettimeofday\n");
   //my
-  return _syscall_(SYS_gettimeofday, (intptr_t)&tv, (intptr_t)&tz, 0);
+  return _syscall_(SYS_gettimeofday, (intptr_t)tv, (intptr_t)tz, 0);
   //
   // _exit(SYS_gettimeofday);
   // return 0;
