e05b08d1cf30789828e614b97051b4dfaf58d6cb
tracer-ics2022 <tracer@njuics.org>
2023-10-19 22:51:30 +0800

    >  compile NEMU 211220000 张三 Linux zjy-virtual-machine 5.15.0-86-generic #96~20.04.1-Ubuntu SMP Thu Sep 21 13:23:37 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:51:30 up  3:19,  1 user,  load average: 0.16, 0.19, 0.10

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -65,13 +65,14 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   //
   // return 0;
 }
-size_t fb_write(void *buf, size_t offset, size_t len) {
+size_t fb_write(const void *buf, size_t offset, size_t len) {
 // my
   int w = io_read(AM_GPU_CONFIG).width;
   // int h = io_read(AM_GPU_CONFIG).height;
   int x=offset%w;
   int y=offset/w;
-  io_write(AM_GPU_FBDRAW, x, y , buf, len, 1, true);
+  printf("%d     %d\n",x,y);
+  // io_write(AM_GPU_FBDRAW, x, y , buf, len, 1, true);
   return len;
   // int h = io_read(AM_GPU_CONFIG).height / N;
   // int block_size = w * h;
